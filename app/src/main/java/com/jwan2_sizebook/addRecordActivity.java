a77284e97fa0523bc1748fe4ff262d30d1207c65
jwan2 <jwan2@ualberta.ca>
2017-01-17 10:24:16 -0700

    Not even prototype

---
+++ app/src/main/java/com/jwan2_sizebook/addRecordActivity.java
@@ -0,0 +1,15 @@
+package com.jwan2_sizebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addRecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_record);
+
+
+    }
+}
